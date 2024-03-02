363b780881c5dffba0c4de028cee36ec4a102936
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-03-02 22:14:03 +0500

    Added Chapter-02 Exercise

---
+++ Chapter-02/app.js
@@ -0,0 +1,38 @@
+// Q.1
+var userName;
+
+// Q.2
+var myName = "Muhammad Danish";
+
+// Q.3
+var message;
+message = "Hello World";
+alert(message);
+
+// Q.4
+var name = "Muhammad Danish";
+var age = "18";
+var Course = "Web and Mobile Application Development";
+
+alert("Student Name is " + name);
+alert("Student age is " + age);
+alert("Student enrolled in " + Course);
+
+//  Q.5
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\np";
+alert(pizza);
+
+// Q.6
+var email = "muhammad.danish1015@gmail.com";
+alert("My email address is " + email);
+
+// Q.7
+var book = ("A smarter way to learn javaScirpt")
+alert("I am trying to learn from the book " + book)
+
+// Q.8
+document.write("yah! I can write HTML content through javaScript")
+
+// Q.9
+var design = "▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬";
+alert(design);
