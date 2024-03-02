59788fe6b3f0a97304721da5ab7a561616959a19
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-03-02 23:00:40 +0500

    Update app.js

--- Chapter-01/app.js
+++ Chapter-01/app.js
@@ -1,12 +1,12 @@
-// // Q.1
-// alert("Hello");
+// Q.1
+alert("Hello");
 
-// // Q.2
-// alert("Error! Please Inter a valid Password");
+// Q.2
+alert("Error! Please Inter a valid Password");
 
-// // Q.3
-// alert("Welocome to JS Land \n Happy Coding!");
+// Q.3
+alert("Welocome to JS Land \n Happy Coding!");
 
-// // Q.4
-// alert("Welcome to JS Land...");
-// alert("Happy coding! \n Prevent this page creating additional diologs ");
+// Q.4
+alert("Welcome to JS Land...");
+alert("Happy coding! \n Prevent this page creating additional diologs ");
