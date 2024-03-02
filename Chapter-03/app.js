165febc00c5ae5eb828f9daa7111715a63a57daf
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-03-02 23:01:47 +0500

    Added Chapter-03 Exercise

---
+++ Chapter-03/app.js
@@ -0,0 +1,17 @@
+// Q.1
+var age = "18";
+alert("I am " + age + " Years old");
+
+// Q.2
+var N = "14";
+alert("you have visited this sites " + N + " times");
+
+// Q.3
+var birthYear = ("2005")
+document.write("My birth year is " + birthYear) 
+
+// Q.4
+var VisitorName = ("Muhammad Danish")
+var Product = ("t-shirt")
+var quantity = ("5")
+document.write(VisitorName + " ordered " + quantity + " " + Product + " (s) on XYZ clothing store" )
