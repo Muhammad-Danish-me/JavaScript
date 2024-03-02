a60c0d4c59a2d91ca2eeed58408f618daa878c80
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-03-02 23:25:20 +0500

    Added Chapter-04 Exercise

---
+++ Chapter-04/chapter04.js
@@ -0,0 +1,26 @@
+// Q.1
+var name, fName, age;
+
+// Q.2
+// 5 legal Variable Names
+
+var name;
+var fName;
+var full_Name;
+var num1;
+var $name;
+
+// 5 illegal Variable Names
+
+// var 1name
+// var full-name
+// var alert
+// var name fname
+// var #name
+
+// Q.3
+document.write("<h1> Rules for naming JS variables </h1> <br>");
+document.write("Variable names can only contain numbers, $ and _. For example: $my_1stVariable<br>");
+document.write("Variables must begin with a letter, $ or _. For example: $name, _name or name<br>");
+document.write("Variable names are case sensitive<br>");
+document.write("Variable names should not be JS Keywords <br>");
