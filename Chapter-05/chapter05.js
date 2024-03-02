5aebe87b7f2b59de6b0345ce8e5fb70d2a67e204
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-03-03 00:03:28 +0500

    Added Chapter-05 Exercise

---
+++ Chapter-05/chapter05.js
@@ -0,0 +1,50 @@
+// Q.1
+var num1 = 3;
+var num2 = 5;
+var result = num1 + num2;
+alert(result);
+
+document.write("Sum of " + num1 + " and " + num2 + " is " + result + " <br> ");
+
+// Q.2
+var result = num1 - num2;
+document.write("Subtraction of " + num1 + " and " + num2 + " is " + result + "<br>");
+
+var result = num1 * num2;
+document.write("multiplication of " + num1 + " and " + num2 + " is " + result + "<br>");
+
+var result = num1 / num2;
+document.write("division of " + num1 + " and " + num2 + " is " + result + "<br>");
+
+var result = num1 * num2;
+document.write("modulus of " + num1 + " and " + num2 + " is " + result + "<br>");
+
+
+// Q.3
+var num
+document.write("Value after variable declaration is " + num + "<br>")
+num = 5
+document.write("Initial value: " + num + "<br>")
+num++
+document.write("Value after increment is: " + num + "<br>")
+num = num + 7
+document.write("Value after addition is: " + num + "<br>")
+num--
+document.write("Value after decrement is " + num + "<br>")
+num = num % 3
+document.write("The remainder is : " + num + "<br>")
+
+
+// Q.4
+var ticket = 600
+var ticketPrice = ticket * 5
+
+document.write("total cost of buy 5 tickets to a movie is " + ticketPrice + "PKR")
+
+
+// Q.5
+
+
+
+
+
