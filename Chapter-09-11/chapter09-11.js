7a20b8ad31d9dbfe77ab4f7131b53af2506cd4ef
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-03-05 13:49:08 +0500

    Added Chapter-09-11 Exercise

---
+++ Chapter-09-11/chapter09-11.js
@@ -0,0 +1,38 @@
+// // Q.1
+// var cityName = prompt("Enter your city name: ");
+// if (cityName === 'karachi' || cityName === 'Karachi') {
+//     alert("welcome to city of lights");
+// } else {
+//     alert("Welcome to " + cityName);
+// }
+
+
+// // Q.2
+// var gender = prompt("Enter your Gender: ")
+// if (gender === "male" ) {
+//     alert ("Good Morning Sir")
+// }
+// else if (gender === "female") {
+//     alert ("Good Morning Ma’am")    
+// }
+// else {
+//     alert("enter your " + gender)
+// }
+
+
+// Q.3
+var color = prompt("Enter color of road traffic signal: ")
+if (color === "red") {
+    alert ("Must Stop")
+}
+else if (color === "yellow") {
+    alert("ready to move")
+}
+else if (color === "green"){
+    alert("move now")
+}
+else {
+    alert("invalid color")
+}
+
+
