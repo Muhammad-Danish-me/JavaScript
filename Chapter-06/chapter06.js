8e73a412a5b550de151590cfe87cd0ff01c01ca9
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-03-05 12:52:20 +0500

    Update chapter06.js

--- Chapter-06/chapter06.js
+++ Chapter-06/chapter06.js
@@ -29,3 +29,23 @@ document.write("your result is " + result )  // Result is 3
 var userName = prompt("Enter your Name")
 
 alert("Assalam u alaikum " + userName )
+
+
+// Q.6
+var sub1 = prompt("enter your first subject name")
+var sub2 = prompt("enter your second subject name")
+var sub3 = prompt("enter your third subject name")
+
+var totalMarks = 100
+
+var obtainedMarks1 = prompt ("enter obtained marks for " + sub1 + ":")
+var obtainedMarks2 = prompt ("enter obtained marks for " + sub2 + ":")
+var obtainedMarks3 = prompt ("enter obtained marks for " + sub3 + ":")
+
+
+document.write("<table border = '1'>");
+document.write("<tr> <th> Subject </th> <th> Total Marks </th> <th> Obtained Marks </th> <th> Percentage </th> </tr>");
+document.write("<tr> <td> " + sub1 + " </td> <td> " + totalMarks + " </td> <td> " + obtainedMarks1 + " </td> <td> " + (obtainedMarks1 / totalMarks) * 100 + "% </td> </tr>");
+document.write("<tr> <td> " + sub2 + " </td> <td> " + totalMarks + " </td> <td> " + obtainedMarks2 + " </td> <td> " + (obtainedMarks2 / totalMarks) * 100 + "% </td> </tr>");
+document.write("<tr> <td> " + sub3 + " </td> <td> " + totalMarks + " </td> <td> " + obtainedMarks3 + " </td> <td> " + (obtainedMarks3 / totalMarks) * 100 + "% </td> </tr>");
+document.write("</table>")
