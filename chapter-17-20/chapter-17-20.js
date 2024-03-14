fe1900ffee3c7e7cfc86eaca0775baaff3a35441
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-03-14 12:12:49 +0500

    Added Chapter-17-20 Exercise
    incomplete

---
+++ chapter-17-20/chapter-17-20.js
@@ -0,0 +1,20 @@
+// // Q.1
+// var myArray = [[], []]; // multidimensional array
+
+
+
+// // Q.2
+// var multiArrays = [
+//     [0,1,2,3]
+//     [1,0,1,2]
+//     [2,1,0,1]
+// ];
+
+
+
+// Q.3
+var counting = [1,2,3,4,5,6,7,8,9,10]
+
+for (var i = 0; i < counting.length; i++){
+    console.log(counting[i] + "\n")
+}
