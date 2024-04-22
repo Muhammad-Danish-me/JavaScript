c16120bd5f52b5ad34e54c02c1d6f60f858b8f6f
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-04-22 20:15:09 +0500

    Update chapter-26-30.js

--- chapter-26-30/chapter-26-30.js
+++ chapter-26-30/chapter-26-30.js
@@ -1,59 +1,59 @@
-// //  Q.1
+//  Q.1
 
-// var positiveInteger = +prompt("Enter a positive integer number");
+var positiveInteger = +prompt("Enter a positive integer number");
 
-// var roundOff = Math.round(positiveInteger);
-// var floor = Math.floor(positiveInteger);
-// var ceil = Math.ceil(positiveInteger);
+var roundOff = Math.round(positiveInteger);
+var floor = Math.floor(positiveInteger);
+var ceil = Math.ceil(positiveInteger);
 
-// console.log("number:" + positiveInteger)
-// console.log("Round Off:" + roundOff)
-// console.log("Floor:" + floor)
-// console.log("Ceil:" + ceil)
-
-
-// // Q.2
-// var negativeInteger = +prompt("Enter a Negative floating point number");
-
-// var roundOff2 = Math.round(negativeInteger);
-// var floor2 = Math.floor(negativeInteger);
-// var ceil2 = Math.ceil(negativeInteger);
-
-// console.log("Number:" + negativeInteger)
-// console.log("Round off:" + roundOff2)
-// console.log("Floor:" + floor2)
-// console.log("Ceil:" + ceil2)
+console.log("number:" + positiveInteger)
+console.log("Round Off:" + roundOff)
+console.log("Floor:" + floor)
+console.log("Ceil:" + ceil)
 
 
-// //  Q.3
+// Q.2
+var negativeInteger = +prompt("Enter a Negative floating point number");
+
+var roundOff2 = Math.round(negativeInteger);
+var floor2 = Math.floor(negativeInteger);
+var ceil2 = Math.ceil(negativeInteger);
+
+console.log("Number:" + negativeInteger)
+console.log("Round off:" + roundOff2)
+console.log("Floor:" + floor2)
+console.log("Ceil:" + ceil2)
 
 
-
-// // Q.4
-//  var dice = Math.floor(Math.random() * 6 + 1) 
-
-//  console.log("Random Dice Value: " + dice)
-
-
-// //  Q.5
-// var coinsValue = Math.floor(Math.random() * 2 + 1)
-
-// if (coinsValue === 1){
-//     console.log(coinsValue)
-//     console.log("Random Coin Value: Tails")
-// }else if(coinsValue === 2){
-//     console.log(coinsValue)
-//     console.log("Random Coin Value: Heads")
-// }else{
-//     console.log("Error the number is not 1 or 2")
-// }
+//  Q.3
 
 
 
-// // Q.6
-// var randomNumber = Math.floor(Math.random() * 100)
+// Q.4
+ var dice = Math.floor(Math.random() * 6 + 1) 
 
-// console.log("Random number between 1 and 100: " + randomNumber)
+ console.log("Random Dice Value: " + dice)
+
+
+//  Q.5
+var coinsValue = Math.floor(Math.random() * 2 + 1)
+
+if (coinsValue === 1){
+    console.log(coinsValue)
+    console.log("Random Coin Value: Tails")
+}else if(coinsValue === 2){
+    console.log(coinsValue)
+    console.log("Random Coin Value: Heads")
+}else{
+    console.log("Error the number is not 1 or 2")
+}
+
+
+
+// Q.6
+var randomNumber = Math.floor(Math.random() * 100)
+
+console.log("Random number between 1 and 100: " + randomNumber)
 
 
 
@@ -67,4 +67,16 @@ if(!isNaN(convertToNumber)){
 }else{
     console.log("Error: Please enter a number");
 
+}
+
+
+// Q.8
+var userInput = +prompt("guess Number between 1 to 10")
+var secretNum = Math.floor(Math.random() * 10)
+
+if (userInput === secretNum){
+    console.log("congratulate for Correct guess")
+}else{
+    console.log(secretNum)
+    console.log("Wrong! Better luck Next time.")
 }
