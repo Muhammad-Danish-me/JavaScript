0318da0ea8926c2ed86bd4a641c03592de687c3d
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-05-25 17:27:56 +0500

    Chapter-43-48 Exercise

---
+++ chapter-43-48/chapter-43-48.js
@@ -0,0 +1,26 @@
+// Events
+
+// Q.1
+function displayAlert() {
+  alert("Hello Sir How are you?");
+}
+//Q.2
+function showAlert() {
+  alert("Thanks For Purchasing a phone from us ");
+}
+// Q.3
+function deleteRow(r) {
+  let i = r.parentNode.parentNode.rowIndex;
+  document.getElementById("myTable").deleteRow(i);
+}
+
+// Q.4
+function swapImage(type) {
+  if (type == "over") {
+    event.target.src =
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSui31baHRlQ7kG9Oa_udoomRgONvhdmICdMhZ5U7_40A&s";
+  } else {
+    event.target.src =
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFkGhb9HYYqqIyNySCcQYTthS4scq3om-u7Ygsj5Tc_g&s";
+  }
+}
