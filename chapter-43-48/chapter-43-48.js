801b3adf0fc4e8c0f566ce29bafc9fb3720cf9fd
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-05-25 17:46:06 +0500

    Event Exercise

--- chapter-43-48/chapter-43-48.js
+++ chapter-43-48/chapter-43-48.js
@@ -24,3 +24,20 @@ function swapImage(type) {
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFkGhb9HYYqqIyNySCcQYTthS4scq3om-u7Ygsj5Tc_g&s";
   }
 }
+
+// Q.5
+var display = document.getElementById("display");
+var increament = document.getElementById("incre");
+var decreament = document.getElementById("decre");
+
+
+function increase() {
+  display.innerText++;
+}
+
+function decrease() {
+  display.innerText--;
+}
+function reset(){
+    display.innerText = "0"
+}
