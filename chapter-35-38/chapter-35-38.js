2872f40526f71df11ed2a3db07dd50ec8672ded6
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-05-25 16:32:15 +0500

    Update chapter-35-38.js

--- chapter-35-38/chapter-35-38.js
+++ chapter-35-38/chapter-35-38.js
@@ -52,3 +52,52 @@ function calculator(num1, num2, opr) {
 }
 
 // calculator();
+
+
+// Q.5
+function square(sq) {
+  sq = +prompt("Enter a number to find its square");
+  var res = sq * sq;
+  return res;
+}
+
+// var squareValue = square();
+// alert("Square of given number is: " + squareValue);
+
+
+// Q.6
+function factorial(num) {
+  num = +prompt("Enter a number to find it's factorial");
+  var res = 1;
+  for (var i = num; i >= 1; i--) {
+    res = res * i;
+  }
+  return res;
+}
+
+// var factorialValue = factorial();
+// alert("Factorial of given number is: " + factorialValue);
+
+// Q.7
+function counting(start, end) {
+  start = +prompt("Enter starting number");
+  end = +prompt("Enter ending number");
+  for (var i = start; i <= end; i++) {
+    document.write(i + "<br/>");
+  }
+  return;
+}
+
+// counting();
+
+// Q.10
+function palindrome(str) {
+  if (str === str.split("").reverse().join("")) {
+    alert("Given string is palindrome: " + str);
+  } else {
+    alert("Given string is not palindrome: " + str);
+  }
+}
+
+var strValue = prompt("Enter a string to check if it is palindrome or not");
+palindrome(strValue);
